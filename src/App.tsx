f26947ea6fe9d4d40e5dc8950ba6d9b03c360a4c
berlysia <berlysia@gmail.com>
2022-08-10 03:08:56 +0900

    online

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,60 @@
-import { useState } from 'react'
+import { useRef, useState } from "react";
+import { onlineSort } from "./onlineSort";
+
+type State<T> = { left: T; right: T };
 
 function App() {
-  const [count, setCount] = useState(0)
+  type T = number;
+  const [state, setState] = useState<IteratorResult<State<T>> | null>(null);
+  const genRef = useRef<Generator<
+    { left: T; right: T },
+    T[],
+    -1 | 0 | 1
+  > | null>(null);
+
+  function handleStart() {
+    genRef.current = onlineSort(
+      Array.from({ length: 5 }, (_, i) => i + 1).sort(() => Math.random() - 0.5)
+    );
+    setState(genRef.current.next());
+  }
+
+  function handleLeftBigger() {
+    setState(genRef.current!.next(-1));
+  }
+
+  function handleRightBigger() {
+    setState(genRef.current!.next(1));
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <div>
+      <div>
+        {state ? (
+          state.done ? (
+            `done!: ${state.value}`
+          ) : (
+            <div>
+              <div>
+                {state.value.left}, {state.value.right}
+              </div>
+              より先頭に来てほしい方を選ぶ
+              <button type="button" onClick={handleLeftBigger}>
+                left
+              </button>
+              <button type="button" onClick={handleRightBigger}>
+                right
+              </button>
+            </div>
+          )
+        ) : (
+          <button type="button" onClick={handleStart}>
+            start
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { sort } from './sort'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
-console.log(
-  sort([7, 5, 3, 5, 6, 2, 3, 4, 5, 8, 9], (a: number, b: number) => a < b ? -1 : a > b ? 1 : 0)
-);
